44a82385603dfd264098a26e0cdca6eb158736d6
rida-hdj <rico777x@gmail.com>
2026-01-20 23:11:25 +0100

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,33 +1,33 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-	highlight = {
-	enable = true,
-	},
-	indent = { enable = true },
-	ensure_installed = {
-	-- Low level
-	    "c", "cpp", "rust", "asm",
-	
-	-- System / Linux configs
-	    "bash", "ini", "toml", "json",
-	
-	-- NixOS language
-	    "nix",
-	
-	-- Web Dev
-	    "html", "css", "javascript", "typescript",
-	    "yaml", "markdown", "markdown_inline",
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            highlight = {
+                enable = true,
+            },
+            indent = { enable = true },
+            ensure_installed = {
+                -- Low level
+                "c", "cpp", "rust", "asm",
 
-	-- Optional but useful
-	    "lua", "vim", "query"
+                -- System / Linux configs
+                "bash", "ini", "toml", "json",
 
-	},
-	auto_install = false,
+                -- NixOS language
+                "nix",
 
-		})
-	end
+                -- Web Dev
+                "html", "css", "javascript", "typescript",
+                "yaml", "markdown", "markdown_inline",
+
+                -- Optional but useful
+                "lua", "vim", "query"
+
+            },
+            auto_install = false,
+
+        })
+    end
 }
