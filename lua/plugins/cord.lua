3715bfe7d6c210c7377281d878353fb8b1fbe011
rida-hdj <rico777x@gmail.com>
2026-01-21 02:16:50 +0100

    Update cord.lua

--- lua/plugins/cord.lua
+++ lua/plugins/cord.lua
@@ -1,14 +1,25 @@
 return {
-  "vyfor/cord.nvim",
-  event = "VeryLazy",
-  config = function()
-    require("cord").setup({
-      main_image = "neovim",
-      neovim_image_text = "Editing in Neovim",
-      editing_text = "Editing %s",
-      file_explorer_text = "Browsing %s",
-      git_commit_text = "Committing changes",
-      buttons = {},
-    })
-  end
+    "vyfor/cord.nvim",
+    event = "VeryLazy",
+    config = function()
+        require('cord').setup({
+            display = {
+                theme = "classic",
+                flavor = "accent",
+                view = "full",
+                swap_fields = true,
+            },
+            timestamp = { enabled = true },
+            idle = {
+                enabled = true,
+                timeout = 60000,
+                show_status = true,
+                ignore_focus = true,
+                unidle_on_focus = true,
+                smart_idle = true,
+                details = "Idling",
+                tooltip = "💤",
+            },
+        })
+    end
 }
