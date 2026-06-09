ba04e6188921d073c458e908981dea572513f841
rida-hdj <rida.hdj@users.noreply.github.com>
2026-06-09 20:40:05 +0100

    live-server

--- lua/plugins/code-runner.lua
+++ lua/plugins/code-runner.lua
@@ -1,29 +1,41 @@
 return {
-  "CRAG666/code_runner.nvim",
-  cmd = {"RunCode", "RunFile", "RunProject", "RunClose"},
-  keys = {
-    {"<leader>r", "<cmd>RunCode<cr>", desc="Run current file"}
-  },
-  config = function()
-    local filetype_commands = {
-      html = "xdg-open $fileName",
-      ts_ls = "ts-node $fileName",
-      pyright = "python3 -u $fileName",
-      rust_analyzer = "cargo run",
-      clangd = "g++ $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
-    }
+    {
+        "CRAG666/code_runner.nvim",
+        cmd = { "RunCode", "RunFile", "RunProject", "RunClose" },
+        keys = {
+            { "<leader>r", "<cmd>RunCode<cr>", desc = "Run current file" }
+        },
+        config = function()
+            local filetype_commands = {
+                html = "xdg-open $fileName",
+                ts_ls = "ts-node $fileName",
+                pyright = "python3 -u $fileName",
+                rust_analyzer = "cargo run",
+                clangd = "g++ $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+            }
 
-    require("code_runner").setup({
-      filetype = filetype_commands,
-      mode = "term",
-      focus = true,
-      startinsert = true,
-      on_run = function(ft)
-        if not filetype_commands[ft] then
-          return false
+            require("code_runner").setup({
+                filetype = filetype_commands,
+                mode = "term",
+                focus = true,
+                startinsert = true,
+                on_run = function(ft)
+                    if not filetype_commands[ft] then
+                        return false
+                    end
+                    return true
+                end
+            })
         end
-        return true
-      end
-    })
-  end
+    },
+    {
+        "https://forge.barrettruth.com/barrettruth/live-server.nvim.git",
+        cmd = { "LiveServerStart", "LiveServerStop", "LiveServerToggle" },
+        config = function()
+            vim.g.live_server = {
+                port = 8080,
+                host = "127.0.0.1",
+            }
+        end,
+    }
 }
