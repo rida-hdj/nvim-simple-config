d52d32c034d04883e2f268c43b8dbc3734789f73
rida-hdj <rida.hdj@users.noreply.github.com>
2026-06-16 16:22:57 +0100

    treesitter-warning

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,18 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ":TSUpdate",
-    config = function()
-        local ok, ts_configs = pcall(require, "nvim-treesitter.configs")
-        if not ok then
-            vim.notify("nvim-treesitter not installed yet!", vim.log.levels.WARN)
-            return
-        end
-
-        ts_configs.setup({
-            highlight = { enable = true },
-            indent = { enable = true },
-            ensure_installed = {
-                "c", "cpp", "rust", "asm",
-                "bash", "ini", "toml", "json",
-                "nix",
-                "html", "css", "javascript", "typescript",
-                "yaml", "markdown", "markdown_inline",
-                "lua", "vim", "query"
-            },
-            auto_install = false,
-        })
-    end
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+            "c", "cpp", "rust", "asm",
+            "bash", "ini", "toml", "json",
+            "nix",
+            "html", "css", "javascript", "typescript",
+            "yaml", "markdown", "markdown_inline",
+            "lua", "vim", "query"
+        },
+        auto_install = false,
+    },
 }
