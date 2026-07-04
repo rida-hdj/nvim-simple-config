a1b9b76bb30001646333c6a3d88d287cd3df8399
rida-hdj <rida.hdj@users.noreply.github.com>
2026-07-04 23:25:01 +0100

    fix toggle term lazy

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,39 +1,47 @@
-return{
-  "akinsho/toggleterm.nvim",
-  opts = {
-    direction = "horizontal",
-    size = 10,
-    start_in_insert = true,
-    persist_mode = true,
-    close_on_exit = true,
-  },
-  config = function(_, opts)
-    require("toggleterm").setup(opts)
+return {
+    "akinsho/toggleterm.nvim",
 
-    local map = vim.keymap.set
+    keys = {
+        {
+            "<C-t>",
+            "<cmd>ToggleTerm<cr>",
+            desc = "Toggle Terminal",
+        },
+    },
 
-    map("n", "<C-t>", "<cmd>ToggleTerm<cr>", { silent = true })
+    opts = {
+        direction = "horizontal",
+        size = 10,
+        start_in_insert = true,
+        persist_mode = true,
+        close_on_exit = true,
+    },
 
-    map("t", "<Esc>", [[<C-\><C-n>]], { silent = true })
+    config = function(_, opts)
+        require("toggleterm").setup(opts)
 
-    map("n", "<leader>q", function()
-      local buf = vim.api.nvim_get_current_buf()
-      local buftype = vim.bo[buf].buftype
+        local map = vim.keymap.set
 
-      if buftype == "terminal" then
-        vim.cmd("bd!")
-        return
-      end
+        map("t", "<Esc>", [[<C-\><C-n>]], { silent = true })
 
-      local bufs = vim.tbl_filter(function(b)
-        return vim.api.nvim_buf_is_loaded(b) and vim.bo[b].buflisted
-      end, vim.api.nvim_list_bufs())
+        map("n", "<leader>q", function()
+            local buf = vim.api.nvim_get_current_buf()
+            local buftype = vim.bo[buf].buftype
 
-      if #bufs <= 1 then
-        vim.cmd("qa")
-      else
-        vim.cmd("bd")
-      end
-    end, { silent = true })
-  end,
+            if buftype == "terminal" then
+                vim.cmd("bd!")
+                return
+            end
+
+            local bufs = vim.tbl_filter(function(b)
+                return vim.api.nvim_buf_is_loaded(b) and vim.bo[b].buflisted
+            end, vim.api.nvim_list_bufs())
+
+            if #bufs <= 1 then
+                vim.cmd("qa")
+            else
+                vim.cmd("bd")
+            end
+        end, { silent = true })
+    end,
 }
