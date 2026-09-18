38dbe634793a635ca53f1e5bdbd10e312be1048f
rida-hdj <rida-hdj@proton.me>
2026-09-18 23:47:29 +0100

    configuring render-markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,7 +2,25 @@ return {
     {
         "MeanderingProgrammer/render-markdown.nvim",
         event = "BufReadPost",
-        opts = {},
+        opts = {
+            heading = {
+                width = "block",
+                right_pad = 2,
+                backgrounds = {
+                    "RenderMarkdownH1Bg",
+                    "RenderMarkdownH2Bg",
+                    "RenderMarkdownH3Bg",
+                    "RenderMarkdownH4Bg",
+                    "RenderMarkdownH5Bg",
+                    "RenderMarkdownH6Bg",
+                },
+            },
+            code = {
+                width = "block",
+                left_pad = 2,
+                right_pad = 2,
+            },
+        },
         config = function(_, opts)
             require("render-markdown").setup(opts)
             vim.keymap.set(
